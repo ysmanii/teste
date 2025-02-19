e7a2e67cbf146d8211964b7c2adf246a64d7433f
ysmani <dippssimara@gmail.com>
2025-02-19 08:39:25 -0300

    adicionando perguntas

---
+++ perguntas-copia.py
@@ -0,0 +1,22 @@
+
+perguntas = [
+    {"pergunta": "Em que ano o Brasil foi descoberto?",
+     "opções": ["1305", "1500", "1494", "1560"],
+     "resposta": "1500"
+    },
+
+    {"pergunta": "Quem foi o primeiro presidente do Brasil?",
+     "opções": ["Bolsonaro", "Lula", "Deodoro Da Fonseca", "Dom Pedro 1"],
+     "resposta": "Deodoro Da Fonseca"
+    },
+
+    {"pergunta": "O sol é um:",
+     "opções": ["satélite", "uma lua", "um planeta", "uma estrela"],
+     "resposta": "uma estrela"
+    },
+
+    {"pergunta": "Qual movimento que a Terra faz em relação ao Sol?",
+     "opções": ["Translação", "rotação", "movimentação"],
+     "resposta": "Translação"
+    },
+]
